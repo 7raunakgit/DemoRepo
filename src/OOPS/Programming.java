00b69f026249bae5b76f10570c3cff8f2d8d019c
Raunak Chowdhury <raunak.chowdhury@seamless.se>
2022-07-09 12:08:43 +0530

    2nd git commit

--- src/OOPS/Programming.java
+++ src/OOPS/Programming.java
@@ -4,13 +4,21 @@ public class Programming {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="aaabbcccczzzzzySSSSSSSS";
-		String a[]=s.split("");
-		String res="";
+		String s="aaabbcccczzzzzySS";
 		
+		int count=1;
 		
-		
-	      System.out.println(res);
+		for(int i=1; i<s.length(); i++) {
+			char curr=s.charAt(i);
+			char prev=s.charAt(i-1);
+			if(curr == prev) {
+				count++;
+			}else {
+				System.out.print(prev+""+count);
+				count=1;
+			}
+		}
+		System.out.print(s.charAt(s.length()-1)+""+count);
 	}
 
 }
